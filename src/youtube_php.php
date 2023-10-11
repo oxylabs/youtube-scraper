e2ac054ff5e6cb177aa11449b34e5805f306741e
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-10-11 17:33:29 +0300

    Create youtube_php.php

---
+++ src/youtube_php.php
@@ -0,0 +1,28 @@
+<?php
+
+$params = array(
+  'source' => 'universal',
+  'url' => 'https://www.youtube.com/watch?v=SLSGtgKWzxg',
+  'render' => 'html'
+);
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://realtime.oxylabs.io/v1/queries");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_USERPWD, "USERNAME" . ":" . "PASSWORD"); //Your credentials go here
+
+$headers = array();
+$headers[] = "Content-Type: application/json";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+echo $result;
+
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
+?>
