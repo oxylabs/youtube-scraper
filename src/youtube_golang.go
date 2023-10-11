af0f7fc5b2b19599e2a8eb4acab274adc55f09d0
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-10-11 17:34:51 +0300

    Create youtube_golang.go

---
+++ src/youtube_golang.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+)
+
+func main() {
+    const Username = "USERNAME"
+    const Password = "PASSWORD"
+
+    payload := map[string]string{
+      	"source": "universal",
+	"url": "https://www.youtube.com/watch?v=SLSGtgKWzxg",
+	"render": "html",
+          }
+
+    jsonValue, _ := json.Marshal(payload)
+
+    client := &http.Client{}
+    request, _ := http.NewRequest("POST",
+        "https://realtime.oxylabs.io/v1/queries",
+        bytes.NewBuffer(jsonValue),
+    )
+
+    request.SetBasicAuth(Username, Password)
+    response, _ := client.Do(request)
+
+    responseText, _ := ioutil.ReadAll(response.Body)
+    fmt.Println(string(responseText))
+}
