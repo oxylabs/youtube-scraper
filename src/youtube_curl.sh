ee853ee5a0b5ae4f9ae053587f0ec484d46f83f8
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-10-11 17:32:40 +0300

    Update youtube_curl.sh

--- src/youtube_curl.sh
+++ src/youtube_curl.sh
@@ -1,2 +1,4 @@
-curl --user USERNAME:PASSWORD 'https://realtime.oxylabs.io/v1/queries' -H "Content-Type: application/json"
+curl --user USERNAME:PASSWORD \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
 -d '{"source": "universal", "url": "https://www.youtube.com/watch?v=SLSGtgKWzxg", "render": "html"}'
