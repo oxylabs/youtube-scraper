c83d7a2389abb1c5936c379943891e551da9dbc6
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-10-11 17:34:19 +0300

    Create youtube_csharp.cs

---
+++ src/youtube_csharp.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace OxyApi
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            const string Username = "USERNAME";
+            const string Password = "PASSWORD";
+
+            var parameters = new Dictionary&lt;string, string>()
+            {
+		{"source", "universal"},
+		{"url", "https://www.youtube.com/watch?v=SLSGtgKWzxg"},
+		{"render", "html"}
+            };
+
+            var client = new HttpClient();
+
+            Uri baseUri = new Uri("https://realtime.oxylabs.io");
+            client.BaseAddress = baseUri;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, "/v1/queries");
+            requestMessage.Content = JsonContent.Create(parameters);
+
+            var authenticationString = $"{Username}:{Password}";
+            var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.ASCIIEncoding.UTF8.GetBytes(authenticationString));
+            requestMessage.Headers.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
+
+            var response = await client.SendAsync(requestMessage);
+            var contents = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(contents);
+        }
+    }
+}
