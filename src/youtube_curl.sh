20ca7b728fe0d14ce8f3775a0c3a09d9afea0f04
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-10-11 17:31:44 +0300

    Create youtube_curl.sh

---
+++ src/youtube_curl.sh
@@ -0,0 +1,2 @@
+curl --user USERNAME:PASSWORD 'https://realtime.oxylabs.io/v1/queries' -H "Content-Type: application/json"
+-d '{"source": "universal", "url": "https://www.youtube.com/watch?v=SLSGtgKWzxg", "render": "html"}'
